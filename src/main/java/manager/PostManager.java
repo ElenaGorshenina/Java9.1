a337b434f97b2d235c6943599ab3cfb8057c16fa
Elena Gorshenina <elenkagorshenina19.07.14@gmail.com>
2021-07-21 23:30:43 +0300

    Added PostManager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,15 @@
+package manager;
+
+import domain.PostService;
+
+public class PostManager {
+    private PostService[]postServices;
+
+    public void wallDelete(String idUrl,String idPost){}
+
+    public PostService[] setSearchQuery(String query) {return null;}//ввод запроса
+
+    public PostService[] getSearchQuery(String query, String idUrl, String domain, Boolean ownersOnly, int count, int offset) {
+        return null;
+    }
+}
